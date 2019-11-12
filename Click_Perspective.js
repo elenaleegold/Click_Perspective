ed3d2a446e1bfd2f4aee97eedabafd7dad023b2f
elenaleegold <eleegold@gmail.com>
2019-11-11 19:02:33 -0500

    added mad face icon

--- Click_Perspective.js
+++ Click_Perspective.js
@@ -8,13 +8,29 @@ let state;
 let r,g,b;
 let canvas;
 let wim;
+var rand;
+var fontMove;
+var redVar, redDiv;
 
 function preload(){
-  wim = loadImage('images/icon2.png')
+  wim = loadImage('https://raw.githubusercontent.com/elenaleegold/Click_Perspective/master/images/icon2.png')
 }
 
 function setup(){
   speed = 0;
+  rand = 0;
+  redVar = 0;
+  fontMove = false;
+  redDiv = createDiv();
+      $('#defaultCanvas0').css("box-sizing", "border-box");
+    // $('#defaultCanvas0').css("height", "800px");
+    $('#defaultCanvas0').css("position", "fixed");
+    $('#defaultCanvas0').css("z-index", "10000");
+    $('#defaultCanvas0').css("margin", "0");
+    $('#defaultCanvas0').css("float", "left");
+    $('#defaultCanvas0').css("bottom","0px");
+    $('#defaultCanvas0').css("left","0px");
+     $('#defaultCanvas0').css("pointer-events","none");
   // jQuery('<div/>', {
   // id: 'siteBox',
   // title: 'div-border'
@@ -31,31 +47,112 @@ function draw(){
   clear();
   createCanvas(windowWidth, windowHeight);
   image(wim, windowWidth - wim.width - 20, 20);
-  if (mouseY >= 20 && mouseY <= 20 + wim.height && mouseX > windowWidth - wim.width - 20 && mouseX < windowWidth - 20 && mouseIsPressed) {
+
+
+  // if (mouseY >= 20 && mouseY <= 20 + wim.height && mouseX > windowWidth - wim.width - 20 && mouseX < windowWidth - 20 && mouseIsPressed) {
+  //     wim.resize(20, 44);
+  //     // bigFont();
+  //     // redTint();
+  //     // blurBaby();
+  //     changeText();
+
+  // } else {
+  //     wim.resize(39, 86);
+  // }
+
+  if (mouseIsPressed) {
       wim.resize(20, 44);
+      // bigFont();
+      // redTint();
+      // blurBaby();
+      // changeText();
+      imageStretch();
   } else {
       wim.resize(39, 86);
   }
 
+  if(fontMove == true){
+     var txtObjs = selectAll('p');
+     for(var i = 0; i < txtObjs.length; i++){
+      txtObjs[i].style('color','rgb('+ int(random(255)) + ',' + int(random(255)) + ',' + int(random(255)) + ')');
+     }
+      // select('p').position(100, 100);
+      // bodyTxt[i].style('font-size',int(random(30) + 'px'));
+      // console.log(bodyTxt[i]);
+  }
+
+
+  if(redVar == 2){
+        redDiv.id('redDiv');
+        redDiv.style('width','100%');
+        redDiv.style('height','100%');
+        redDiv.style('background-color','red');
+        redDiv.style('position','fixed');
+        redDiv.style('z-index','100');
+        redDiv.style('mix-blend-mode','multiply');
+        redVar = 1;
+  }
+  else if(redVar == 1){
+    var randNum = random(100,255);
+    console.log(randNum);
+    redDiv.style('background-color','rgb(' + randNum + ',0,0');
+  }
+
+
 }
 
-function coolDown(num, links){
-  console.log("hey!");
-   for(var i = 0; i < links.length; i++){
-    let fontS = map(num, 10, 0, 60, 12);
-    links[i].style('font-size', fontS + 'px');
-    links[i].style('pointer-events','none');
-    links[i].style('cursor','default');
+function bigFont(){
+  fontMove = true;
+  // body.style('font-size','100px');
+}
+
+function redTint(){
+      redVar = 2;
+}
+
+function blurBaby(){
+  var body = select('body');
+  body.style('filter','blur(4px)');
+}
+
+function changeText(){
+  var p = selectAll('p');
+  var p2 = selectAll('p');
+  var x = p[12].elt.innerText;
+  for(var i = 0; i < p.length; i++){
+    var rand = int(random(p.length));
+
+    p[i].html(p2[rand].elt.innerText);
   }
-  let newNum = num--;
-  print(newNum);
-  if (newNum != 0){
-    coolDown(newNum, links);
+
+  var h1 = selectAll('h1');
+  var h2 = selectAll('h1');
+
+  for(var i = 0; i < h1.length; i++){
+    var rand = int(random(h1.length));
+    h1[i].html(h2[rand].elt.innerText);
   }
-  else{
-    return 0;
+
+  var a = selectAll('a');
+  var a2 = selectAll('a');
+
+  for(var i = 0; i < a.length; i++){
+    var rand = int(random(a.length));
+    a[i].html(a2[rand].elt.innerText);
+  }
+
+
+}
+
+function imageStretch(){
+  var img = selectAll('img');
+  for(var i = 0; i < img.length; i++){
+    // var rand = int(random(400));
+    img[i].elt.src = 'https://raw.githubusercontent.com/elenaleegold/Scrolling_Loneliness/master/images/body.png';
+    console.log(img[i]);
   }
 }
+
 
 
 function mouseWheel(event) {
